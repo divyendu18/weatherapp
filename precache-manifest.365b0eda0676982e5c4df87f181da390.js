8530dae464cc728704a64218445291fb7fa85402
divyendu18 <d4divyendu18@gmail.com>
2020-05-19 23:40:41 +0530

    Updates

---
+++ precache-manifest.365b0eda0676982e5c4df87f181da390.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1b2cb0cbcbecab4c683842d1d889f8",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "af742c86ff0675ee969b",
+    "url": "/weatherapp/static/css/2.da2ff830.chunk.css"
+  },
+  {
+    "revision": "bb0006b1b1e6ee8be8e5",
+    "url": "/weatherapp/static/css/main.8ab6c8a3.chunk.css"
+  },
+  {
+    "revision": "af742c86ff0675ee969b",
+    "url": "/weatherapp/static/js/2.6efeeb4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.6efeeb4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb0006b1b1e6ee8be8e5",
+    "url": "/weatherapp/static/js/main.242a0025.chunk.js"
+  },
+  {
+    "revision": "37a461f81c69d6959f2d",
+    "url": "/weatherapp/static/js/runtime-main.5392d643.js"
+  },
+  {
+    "revision": "11b058bc54bc1118ec99e673f148694f",
+    "url": "/weatherapp/static/media/blur-background.11b058bc.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
